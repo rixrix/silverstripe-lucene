a53c1f59ef4f846b2481199a66e23f54977248e0
darren.inwood <darren.inwood@37c722ee-a693-3632-ac79-18467f915a76>
2010-10-27 03:54:16 +0000

    Updated docs
    git-svn-id: http://lucene-silverstripe-plugin.googlecode.com/svn/trunk@14 37c722ee-a693-3632-ac79-18467f915a76

--- _config.php
+++ _config.php
@@ -11,13 +11,6 @@ set_include_path(
 ZendSearchLuceneSearchable::$cacheDirectory = TEMP_FOLDER;
 ZendSearchLuceneSearchable::$encoding = 'utf-8';
 
-// Results per page
-ZendSearchLuceneSearchable::$pageLength = 10;
-// Always show this many pages at the start of the pagination (can be 0)
-ZendSearchLuceneSearchable::$alwaysShowPages = 3;
-// Maximum number of pages to show in pagination (will show ellipses to indicate more pages)
-ZendSearchLuceneSearchable::$maxShowPages = 10;
-
 Zend_Search_Lucene_Search_QueryParser::setDefaultEncoding(ZendSearchLuceneSearchable::$encoding);
 Zend_Search_Lucene_Analysis_Analyzer::setDefault( 
     new StandardAnalyzer_Analyzer_Standard_English() 
